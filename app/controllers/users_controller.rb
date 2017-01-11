222cd5730c44f9d276a0b6d2da0a39f1ea58eff4
entcheva <christina.entcheva@gmail.com>
2017-01-11 18:21:39 -0500

    WIP User CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  # def index
+  # end
+
   def new
     @user = User.new
   end
@@ -13,8 +16,19 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(session[:user_id])
   end
+
+  def edit
+    @user = User.find(session[:user_id])
+  end
+
+  def update
+    @user = User.find(session[:user_id])
+  end
+
+  # def destroy
+  # end
 
   private
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,10 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   belongs_to :user
 
+  def restaurant_name
+    Restaurant.find(self.restaurant_id).name
+  end
+
   def calculate_review_avg
     self.avg_rating = ((food_rating + environment_rating + service_rating) / 3.to_f).round(2)
     save
