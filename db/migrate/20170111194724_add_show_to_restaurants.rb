ba936c0016fa039d40687c00ff6b006c46509c9b
Clinton Nguyen <clinton.ngyn@gmail.com>
2017-01-18 15:50:42 -0500

    oops typo

--- db/migrate/20170111194724_add_show_to_restaurants.rb
+++ db/migrate/20170111194724_add_show_to_restaurants.rb
@@ -6,7 +6,7 @@ class AddShowToRestaurants < ActiveRecord::Migration[5.0]
     add_column :restaurants, :facebook, :string
     add_column :restaurants, :menu, :string
     add_column :restaurants, :photo, :string
-    add_column :restaurants, :price_tier, :integer,
+    add_column :restaurants, :price_tier, :integer
     add_column :restaurants, :last_updated, :datetime
   end
 end
