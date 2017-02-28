7158a76cbd4ffb05a1e0b27ed3d7cedf3d7731ca
Clinton Nguyen <clinton.ngyn@gmail.com>
2017-02-28 13:08:28 -0500

    Some login form styling

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,7 +5,7 @@ class Search < ApplicationRecord
   end
 
   def api_url
-    "https://api.foursquare.com/v2/venues/search?v=20161016&query=#{self.search}&intent=checkin&client_id=#{ENV['foursquare_client_id']}&client_secret=#{ENV['foursquare_client_secret']}&near=#{self.location}&limit=12&categoryId=4d4b7105d754a06374d81259,4d4b7105d754a06376d81259"
+    "https://api.foursquare.com/v2/venues/search?v=20161016&query=#{self.search}&intent=checkin&client_id=#{ENV['foursquare_client_id']}&client_secret=#{ENV['foursquare_client_secret']}&near=#{self.location}&limit=12&categoryId=4d4b7105d754a06374d81259,4d4b7105d754a06376d81259&radius=10000"
   end
 
   def map_restaurants(restaurants)
